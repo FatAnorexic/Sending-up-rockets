aae89ba7c3ac0911f69737d74e71871c4491e803
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-04-11 23:40:45 -0400

    Add basic file writing operation
    This will later be expanded to write full blown spreadsheets of data that can be opened and loaded so that manual input is
    not constantly required.

--- Orbital Meachincs/hohmann_transfer.py
+++ Orbital Meachincs/hohmann_transfer.py
@@ -6,10 +6,11 @@ and the time for half the period of the transfer.
 from scipy.constants import gravitational_constant as G
 import astropy as ast
 import numpy as np
+import csv
 
 target=input('Enter the name of the target: ')
-depart=input("\nEnter the name of the body you're departing from: ")
-central_body=input('\nEnter the name of the central body: ')
+depart=input("Enter the name of the body you're departing from: ")
+central_body=input('Enter the name of the central body: ')
 
 def sma_hohmann(a_target, a_departure):
     #Takes the SMA of both the departure body and the target body, relative to the central mass in the 
@@ -39,25 +40,33 @@ def main():
     #Get the radius of the target body from the central body in the system| for a perfectly circular orbit we only need the 
     #radius r(assuming the center of mass of the major body does not shift) However for any elliptical orbit we need the 
     #semi-major axis
-    a_target=(input(f'\nEnter the semi-major axis in meters of {target} from {central_body}: '))
+    a_target=float(input(f'\nEnter the semi-major axis in meters of {target} from {central_body}: '))
     
     #Get the semi-major axis of the departing body in comparison to the central body of the system. IE Earth to Mars|
     #return the SMA of Earth in comparison to the sun. 
-    a_departing=(input(f'\nEnter the SMA of {depart} in relation to {central_body}: '))
+    a_departing=float(input(f'\nEnter the SMA of {depart} in relation to {central_body}: '))
 
     #Standard Gravitational Constant
     mu=G*m
-
-    print(test(a_target, a_departing, mu))
+    testing=test(a_target, a_departing, mu)
+    csv_write(testing)
+    
 
 def test(a_t, a_d, mu):
-    print(f'\nStandard Gravitational Constant: {mu:.3e}')
-    t_target=period(a_t, mu)
+    print(f'\nStandard Gravitational Constant: {mu:.3e} m**3/s**2\n')
+    t_target=period(a_t, mu) 
     t_depart=period(a_d, mu)
-    t_hoh=period(sma_hohmann(a_t,a_d))
-    print(f'')
-    
-    return 
+    t_hoh=period(sma_hohmann(a_t,a_d), mu)
+    print(f'Perioid of {target}: {t_target:.0f} s')
+    print(f'period of {depart}: {t_depart:.0f} s')
+    print(f'period of hohmann transfer: {t_hoh:.0f} s\n')
+    return phase_angle(t_hoh, t_target)
+
+def csv_write(param):
+    file=open("test.txt", 'w')
+    file.write(f'Phase Angle: {param:.2f}')
+    file.close
+        
 
 if __name__=="__main__":
     main()
